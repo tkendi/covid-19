79daa884674d3101d78eb7582097191a50d76a88
Tkendi <acg70159@gmail.com>
2020-07-29 16:04:58 +0900

    rename word

---
+++ src/components/World/world.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Typography} from '@material-ui/core'
+
+const word = () => {
+    return (
+        <div>
+            <Typography>select</Typography>
+        </div>
+    )
+}
+
+export default word

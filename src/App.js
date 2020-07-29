8948ce7c2f7dc67660831734de69b21811b1a72a
Tkendi <acg70159@gmail.com>
2020-07-29 16:27:18 +0900

    style change

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
 import React from "react";
-import { Cards, Chart, CountryPicker } from "./components";
-import styles from "./App.module.css";
-import { fetchData } from "./api/world";
+import { Route } from "react-router-dom";
+import world from "./components/World/world";
 
-import coronaImage from './images/image.png';
-
-class App extends React.Component {
-  state = {
-    data: {},
-    country: '',
-  };
-
-  async componentDidMount() {
-    const fetchedData = await fetchData();
-
-    this.setState({ data: fetchedData });
-  } 
-
-  handleCountryChange = async(country) => {
-    const fetchedData = await fetchData(country)
-
-    this.setState({data: fetchedData, country: country} )
-  }
-
-  render() {
-    const { data, country } = this.state;
-    return (
-      <div className={styles.container}>
-        <img src = {coronaImage} className = {styles.image} alt = "COVID-19" />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data = {data} country = {country} className = {styles.chart} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <Route path="/world" component={world} />;
+};
 
 export default App;
--- src/components/World/world.js
+++ src/components/World/world.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import {Typography} from '@material-ui/core'
+import React from "react";
+import { Cards, Chart, CountryPicker } from "../public";
+import styles from "./world.module.css";
+import { fetchData } from "../../api/world";
+import coronaImage from '../../images/image.png'
 
-const word = () => {
+class word extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
     return (
-        <div>
-            <Typography>select</Typography>
-        </div>
-    )
+      <div className={styles.container}>
+        <img src={coronaImage} className={styles.image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} className={styles.chart} />
+      </div>
+    );
+  }
 }
 
-export default word
+export default word;

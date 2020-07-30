7d1c40ce919278eb0b937653c525ff042e76699f
Tkendi <acg70159@gmail.com>
2020-07-30 19:54:51 +0900

    korea data parsing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import world from "./components/World/world";
 
 const App = () => {
-  return <Route path="/world" component={world} />;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/world">Word</Link>
+        </li>
+      </ul>
+      <Route path="/world" exact={true} component={world} />
+    </div>
+  );
 };
 
 export default App;

8b7a860cfa83f7cd4731602e395dbeabae10f174
Tkendi <acg70159@gmail.com>
2020-08-05 19:51:15 +0900

    style change

--- src/components/public/CountryPicker/CountryPicker.js
+++ src/components/public/CountryPicker/CountryPicker.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { NativeSelect, FormControl, MenuItem, Select } from "@material-ui/core";
+import { NativeSelect, FormControl } from "@material-ui/core";
 
 import { fetchCountries } from "../../../api/world";
 

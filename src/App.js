47bf927af5f3ad0095eef16bbc59a206245a68c4
Tkendi <acg70159@gmail.com>
2020-08-26 20:38:14 +0900

    news api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 import SideBar from './components/public/SideBar/drawer'
-import {news} from './api/news'
 
 class App extends React.Component {
-  async componentDidMount() {
-    const data = await news()
-    console.log(data)
-  }
   render() {
     return(
       <React.Fragment>
--- src/api/news.js
+++ src/api/news.js
@@ -4,5 +4,5 @@ export const news = () => {
   const newsData = axios.get(
     `https://newsapi.org/v2/top-headlines?country=kr&q=코로나&apiKey=${process.env.REACT_APP_NewsAPI}`
   );
-  console.log(newsData);
+  return newsData;
 };

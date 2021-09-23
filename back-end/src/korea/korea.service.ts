26a42b8c6843c2210cfd609bc612d614a67bc72a
ChaMyungjun <acg70159@gmail.com>
2021-09-23 23:36:42 +0900

    fix world data month

--- back-end/src/korea/korea.service.ts
+++ back-end/src/korea/korea.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class KoreaService {
+  //today korea data
   async getKorea(): Promise<any> {
     const date = new Date();
     date.setHours(date.getHours() - 1);
@@ -27,34 +28,34 @@ export class KoreaService {
 
     const url = `http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=${process.env.CovidOpenDataKey}&pageNo=1&numOfRows=10&startCreateDt=${startDt}&endCreateDt=${endDt}&_type=json`;
 
-    let data;
+    let data: any = [];
 
     await axios
       .get(url)
       .then((res) => {
-        console.log(res);
         data = res.data.response.body.items;
       })
-      .catch((e) => console.log(e));
+      .catch((e) => console.log('error: ', e));
     return data.item;
   }
 
-  // 달 별로 나오는 구간설정
+  //Per Month data setting
   async getKoreaMonth(): Promise<any> {
     const date = new Date();
     date.setHours(date.getHours() - 1);
-    let startDt = `${date.getFullYear()}${('0' + (date.getMonth() - 5)).slice(
-      -2,
-    )}${('0' + date.getDate()).slice(-2)}`;
+    let startDt = `${date.getFullYear()}${('0' + date.getMonth()).slice(-2)}${(
+      '0' + date.getDate()
+    ).slice(-2)}`;
 
     let endDt = `${date.getFullYear()}${('0' + (date.getMonth() + 1)).slice(
       -2,
     )}${('0' + date.getDate()).slice(-2)}`;
 
     if (date.getHours() < 12) {
-      startDt = `${date.getFullYear()}${('0' + (date.getMonth() - 5)).slice(
-        -2,
-      )}${('0' + (date.getDate() - 1)).slice(-2)}`;
+      startDt = `${date.getFullYear()}${('0' + date.getMonth()).slice(-2)}${(
+        '0' +
+        (date.getDate() - 1)
+      ).slice(-2)}`;
 
       endDt = `${date.getFullYear()}${('0' + (date.getMonth() + 1)).slice(
         -2,
@@ -68,7 +69,7 @@ export class KoreaService {
     await axios
       .get(url)
       .then((res) => (data = res.data.response.body.items))
-      .catch((e) => console.log(e));
-    return data?.item.filter((cur: any) => cur?.gubun === '합계');
+      .catch((e) => console.log('error: ', e));
+    return data?.item.filter((cur: any) => cur?.gubun === '합계').slice(0, 32);
   }
 }

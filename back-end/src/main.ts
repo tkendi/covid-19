e1f5fdcfc4d48826462bb394e88bee074e0d5649
ChaMyungjun <acg70159@gmail.com>
2021-10-03 01:28:37 +0900

    add nest swagger ui setting

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import {setupSwagger} from "src/util/swagger"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // setupSwagger(app);
   app.enableCors();
   await app.listen(8000);
 }
-bootstrap();
+
+void bootstrap();

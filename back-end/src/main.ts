5849d9d9ad4c7b6cc0e377e5f462f9b4075972ad
ChaMyungjun <acg70159@gmail.com>
2021-10-03 18:29:58 +0900

    update swagger ui setting

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import {setupSwagger} from "src/util/swagger"
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // setupSwagger(app);
+
+  //cors enable
   app.enableCors();
+
+  //swagger options setting
+  const options = new DocumentBuilder()
+    .setTitle('NestJS Study API Docs')
+    .setDescription('NestJS Study API Description')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  //swagger ui create
+
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(8000);
 }
 

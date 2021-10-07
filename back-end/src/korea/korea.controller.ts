d90f4b7e0307edd281767cda497765f453a99e0f
ChaMyungjun <acg70159@gmail.com>
2021-10-07 20:15:41 +0900

    update controller api swagger tags

--- back-end/src/korea/korea.controller.ts
+++ back-end/src/korea/korea.controller.ts
@@ -1,17 +1,33 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
 import { KoreaService } from './korea.service';
+
 // import { Env } from '../../constants/Env';
 
 @Controller('api/korea')
+@ApiTags('한국 API')
 export class KoreaController {
   constructor(private readonly koreaService: KoreaService) {}
 
   @Get()
+  @ApiOperation({
+    summary: '코로나 한국 데이터 API',
+    description: '코로나 한국 데이터 API.',
+  })
+  @ApiResponse({
+    description: '한국에 대한 시도별 사망자, 확진자, 완치자를 보여준다.',
+  })
   getKorea(): any {
     return this.koreaService.getKorea();
   }
 
   @Get('/month')
+  @ApiOperation({
+    summary: '코로나 한국 1달간 데이터 API',
+    description: '코로나 한국 1달간 데이터 API.',
+  })
+  @ApiResponse({ description: '한국에 대한 1달간 사망자, 확진자 를 보여준다.' })
   getKoreaMonth(): any {
     return this.koreaService.getKoreaMonth();
   }
--- back-end/src/world/world.controller.ts
+++ back-end/src/world/world.controller.ts
@@ -1,11 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
 import { WorldService } from './world.service';
 
 @Controller('api/world')
+@ApiTags('전세계 API')
 export class WorldController {
   constructor(private readonly worldService: WorldService) {}
 
   @Get()
+  @ApiOperation({
+    summary: '코로나 전세계 데이터 API',
+    description: '코로나 전세계 데이터 API.',
+  })
+  @ApiResponse({
+    description: '전세계에 대한 시도별 사망자, 확진자를 보여준다.',
+  })
   getWorld(): any {
     return this.worldService.getWorld();
   }
